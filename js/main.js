5b01410281abd08cdf0df17a6c253766f51bfed6
EvePeralta <eperalta.cm@gmail.com>
2016-10-24 21:14:06 -0300

    arriba el ejercicio, no supe hacer el boton y el checkbox

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("form-signin-heading").value = ""; 
+document.getElementById("form-signin-heading").placeholder = "Por favor inicie sesión"; 
+
+document.getElementById("inputEmail").value ="";
+document.getElementById("inputEmail").placeholder= "Por favor ingrese su email";
+document.getElementById("inputPassword").value = "";
+document.getElementById("inputPassword").placeholder = "Contraseña";
+
+document.getElementById("form-signin-heading").innerHTML = "Por favor Inicie sesión"; 
+
+document.getElementByTagName("input") = "Recuerdame";
